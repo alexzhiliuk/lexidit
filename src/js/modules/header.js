381d46e3244a5c1aa5736ebe0518a72b3519fae5
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-05-15 12:38:20 +0300

    fix

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -9,18 +9,7 @@ $("[data-to-level]").click(function() {
     let level = $(this).attr("data-to-level")
     let menu = $(this).parents("[data-level]")
 
-    if (level != 0) {
-        $(".mobile-menu__info").hide(function() {
-            menu.hide(function() {
-                $("[data-level=" + level + "]").show()
-            })
-        })
-    } else {
-        menu.hide(function() {
-            $("[data-level=" + level + "]").show(function () { 
-                $(".mobile-menu__info").show()
-            })
-        })
-    }
+    menu.hide()
+    $("[data-level=" + level + "]").show()
     
 })
